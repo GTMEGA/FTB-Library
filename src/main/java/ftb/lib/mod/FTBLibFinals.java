c9190b45fc836e8aa5d3c577829cb44ab34c9fdb
matt-159 <16054364+matt-159@users.noreply.github.com>
2023-11-04 09:38:52 -0700

    upgrade to falsepattern's buildscript

--- src/main/java/ftb/lib/mod/FTBLibFinals.java
+++ src/main/java/ftb/lib/mod/FTBLibFinals.java
@@ -2,8 +2,8 @@ package ftb.lib.mod;
 
 public class FTBLibFinals
 {
-	public static final String MOD_ID = "FTBL";
-	public static final String MOD_NAME = "FTBLib";
+	public static final String MOD_ID = "GRADLETOKEN_MODID";
+	public static final String MOD_NAME = "GRADLETOKEN_MODNAME";
 	public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
 	public static final String MOD_DEP = "required-after:Forge@[10.13.4.1558,);after:Baubles;after:NotEnoughItems;after:Waila;after:MineTweaker3";
 }
