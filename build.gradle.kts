34bd7197c61007b0ca1b53eb51ad9282713e0998
FalsePattern <me@falsepattern.com>
2024-12-30 14:29:23 +0100

    update buildscript

---
+++ build.gradle.kts
@@ -0,0 +1,32 @@
+plugins {
+    id("fpgradle-minecraft") version ("0.10.0")
+}
+
+group = "mega"
+
+minecraft_fp {
+    mod {
+        modid = "FTBL"
+        name = "FTBLib"
+        rootPkg = "ftb.lib"
+    }
+    tokens {
+        tokenClass = "Tags"
+    }
+
+    publish {
+        changelog = "https://github.com/GTMEGA/FTB-Library/releases/tag/$version"
+        maven {
+            repoUrl = "https://mvn.falsepattern.com/gtmega_releases"
+            repoName = "mega"
+        }
+    }
+}
+
+repositories {
+    exclusive(horizon(), "com.github.GTNewHorizons")
+}
+
+dependencies {
+    compileOnly("com.github.GTNewHorizons:waila:1.8.2:dev")
+}
--- src/main/java/ftb/lib/mod/FTBLibFinals.java
+++ src/main/java/ftb/lib/mod/FTBLibFinals.java
@@ -1,9 +1,11 @@
 package ftb.lib.mod;
 
+import ftb.lib.Tags;
+
 public class FTBLibFinals
 {
-	public static final String MOD_ID = "GRADLETOKEN_MODID";
-	public static final String MOD_NAME = "GRADLETOKEN_MODNAME";
-	public static final String MOD_VERSION = "GRADLETOKEN_VERSION";
+	public static final String MOD_ID = Tags.MOD_ID;
+	public static final String MOD_NAME = Tags.MOD_NAME;
+	public static final String MOD_VERSION = Tags.MOD_VERSION;
 	public static final String MOD_DEP = "required-after:Forge@[10.13.4.1558,);after:Baubles;after:NotEnoughItems;after:Waila;after:MineTweaker3";
 }
